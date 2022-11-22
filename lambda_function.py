1b61930bbf2677082cfaad883eb2aa3ba26ad79e
Hash7ag <Hash7ag@protonmail.com>
2022-11-22 00:47:06 -0800

    Add lambda function

---
+++ lambda_function.py
@@ -0,0 +1,58 @@
+import ujson as json
+import boto3
+
+def lambda_handler(event, context):
+    
+    BUCKET_NAME = "s3-sha-crackstation"
+    SHA1_FILE_KEY = "sha1-data.json"
+    SHA2_FILE_KEY = "sha2-data.json"
+    
+    try:
+        para = event["queryStringParameters"]["shaHash"]
+    except:
+        return {
+            "statusCode": 400,
+            "body": json.dumps(
+                { "message": "Query parameters (shaHash) not found." }
+            )
+        }
+    
+    if len(para) not in [40, 64]:
+        return {
+            "statusCode": 404,
+            "body": json.dumps(
+                {
+                    "message": "Unable to crack.",
+                    para: None
+                }
+            )
+        }
+    else:
+        s3 = boto3.resource("s3")
+        if len(para) == 40:
+            s3.Bucket(BUCKET_NAME).download_file(SHA1_FILE_KEY, f"/tmp/data.json")
+        else:
+            s3.Bucket(BUCKET_NAME).download_file(SHA2_FILE_KEY, f"/tmp/data.json")
+            
+        with open("/tmp/data.json", "r") as json_file:
+            data = json.loads(json_file.read())
+        
+        result = data.get(para)
+        
+        if result == None:
+            return {
+                "statusCode": 404,
+                "body": json.dumps(
+                    {
+                        "message": "Unable to crack.",
+                        para: None
+                    }
+                )
+            }
+        else:
+            return {
+                'statusCode': 200,
+                'body': json.dumps(
+                    { para: result }
+                )
+            }
